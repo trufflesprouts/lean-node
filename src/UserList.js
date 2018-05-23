04aaaa8ceb359b0a96e2eb5167fc26b8dc2e008e
Abe Baali <milkyabdullah@gmail.com>
2018-05-23 09:27:57 +0300

    Fix user sorting

--- src/UserList.js
+++ src/UserList.js
@@ -19,80 +19,83 @@ export default class UserList extends Component {
 
   render() {
     const { data, updateUser, addUpdateListener } = this.props;
+
+    const sortedData = data.slice(0).sort((a, b) => {
+      if (a.date < b.date) return 1;
+      if (a.date > b.date) return -1;
+      if (a.date === b.date) return 0;
+    });
+
     return (
       <ul className="user-list">
         {data.length < 1 ? <div className="spinner" /> : null}
-        {data &&
-          data.sort((a, b) => a.date < b.date).map(user => (
-            <li key={user.id}>
-              <button
-                className={`apply-changes ${
-                  this.state[user.id] ? '' : 'hidden'
-                }`}
-                onClick={({ target }) => {
-                  target.disabled = true;
-                  updateUser(user.id, this.state[user.id]);
-                  // this function will be called after the next update from the socket
-                  addUpdateListener(() => {
-                    this.setState({ [user.id]: null });
-                    target.disabled = false;
-                  });
-                }}
-              >
-                Save
-              </button>
-              <img src={getImageUrl(user.avatar)} alt="" />
-              <h2>
-                {user.name}, {user.age}
-              </h2>
-              <textarea
-                className="username"
-                value={
-                  (this.state[user.id] && this.state[user.id].username) ||
-                  user.username
-                }
-                onChange={({ target }) => {
-                  this.setState({
-                    [user.id]: {
-                      ...this.state[user.id],
-                      username: target.value
-                    }
-                  });
-                }}
-              />
-              <textarea
-                className="email"
-                value={
-                  (this.state[user.id] && this.state[user.id].email) ||
-                  user.email
-                }
-                onChange={({ target }) => {
-                  this.setState({
-                    [user.id]: {
-                      ...this.state[user.id],
-                      email: target.value
-                    }
-                  });
-                }}
-              />
-              <textarea
-                className="biography"
-                value={
-                  (this.state[user.id] && this.state[user.id].biography) ||
-                  user.biography
-                }
-                onChange={({ target }) => {
-                  this.setState({
-                    [user.id]: {
-                      ...this.state[user.id],
-                      biography: target.value
-                    }
-                  });
-                }}
-              />
-              {/* <span className="biography">{user.biography}</span> */}
-            </li>
-          ))}
+        {sortedData.map(user => (
+          <li key={user.id}>
+            <button
+              className={`apply-changes ${this.state[user.id] ? '' : 'hidden'}`}
+              onClick={({ target }) => {
+                target.disabled = true;
+                updateUser(user.id, this.state[user.id]);
+                // this function will be called after the next update from the socket
+                addUpdateListener(() => {
+                  this.setState({ [user.id]: null });
+                  target.disabled = false;
+                });
+              }}
+            >
+              Save
+            </button>
+            <img src={getImageUrl(user.avatar)} alt="" />
+            <h2>
+              {user.name}, {user.age}
+            </h2>
+            <textarea
+              className="username"
+              value={
+                (this.state[user.id] && this.state[user.id].username) ||
+                user.username
+              }
+              onChange={({ target }) => {
+                this.setState({
+                  [user.id]: {
+                    ...this.state[user.id],
+                    username: target.value
+                  }
+                });
+              }}
+            />
+            <textarea
+              className="email"
+              value={
+                (this.state[user.id] && this.state[user.id].email) || user.email
+              }
+              onChange={({ target }) => {
+                this.setState({
+                  [user.id]: {
+                    ...this.state[user.id],
+                    email: target.value
+                  }
+                });
+              }}
+            />
+            <textarea
+              className="biography"
+              value={
+                (this.state[user.id] && this.state[user.id].biography) ||
+                user.biography
+              }
+              onChange={({ target }) => {
+                this.setState({
+                  [user.id]: {
+                    ...this.state[user.id],
+                    biography: target.value
+                  }
+                });
+              }}
+            />
+            {/* <span className="biography">{user.biography}</span> */}
+          </li>
+        ))}
       </ul>
     );
   }
